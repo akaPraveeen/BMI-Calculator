bbad57f2f84284df4effab55d379329559fe684d
Praveen Kumar <praveeendoc@gmail.com>
2025-07-25 21:00:40 +0530

    cal fix 0.1

--- BMI/src/components/BMI.jsx
+++ BMI/src/components/BMI.jsx
@@ -3,8 +3,8 @@ import { useState } from 'react'
 
 const BMI = () => {
 
-  const [Weight, setWeight] = useState(0);
-  const [Height, setHeight] = useState(0);
+  const [Weight, setWeight] = useState();
+  const [Height, setHeight] = useState();
   const [bmi, setBmi] = useState('');
   const [message, setMessage] = useState('');
 
@@ -12,15 +12,28 @@ const BMI = () => {
   const calBmi = (e) =>{
     
     event.preventDefault()
-    
+    console.log(event);
+
+    // setHeight(Height = Height/100);
+
     if(Weight < 0 || Height <0){
       alert('Please Enter a valid Height and Weight')
     }
     else{
       let bmi = (Weight/(Height*Height));
       setBmi(bmi.toFixed(1))
+
+      // message 
+      if(bmi<18.5)setMessage('Underweight');
+      else if(bmi<25)setMessage('Normal (Healthy Weight)');
+      else if(bmi>25)setMessage('Obese');
     }
   }
+
+// Relode
+ let Relode =()=>{
+  window.location.reload()
+ }
 
   return (
     <div className='container'>
@@ -28,18 +41,17 @@ const BMI = () => {
       <form onSubmit={calBmi}>
         
       <div className="inputs">
-        <p>Enter your weight (Kg)</p>
-        <input type="text" placeholder='Weight' name='Weight' value={Weight} onChange={(e) => setWeight(e.target.value)} />
-        <p>Enter your Height (ft)</p>
-        <input type="text" placeholder='Height' name='Height' value={Height} onChange={(e) => setHeight(e.target.value)} />
+        <input type="text" placeholder='Weight in Kgs' name='Weight' value={Weight} onChange={(e) => setWeight(e.target.value)} />
+        <input type="text" placeholder='Height in Ms' name='Height' value={Height} onChange={(e) => setHeight(e.target.value)} />
       </div>
       <div className="btns">
         <button className='btn'type='submit'>Calculate</button>
-        <button >Relode</button>
+        <button onClick={Relode}>Relode</button>
       </div>
       </form>
       <div className="resutl">
         <h3>Your BMi is:{bmi}</h3>
+        <h3>{message}</h3>
       </div>
     </div>
   )
